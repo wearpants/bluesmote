7105193e19a2286dcb8209ce823407d085128114
Peter Fein <pete@wearpants.org>
2013-10-31 11:56:42 -0500

    write iterload_dir

--- analysis/bandwidth.py
+++ analysis/bandwidth.py
@@ -1,10 +1,15 @@
 from bluesmote import jsontab
 import numpy as np
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 dtype = np.dtype([('timestamp', 'datetime64[s]'), ('outbound', np.uint64), ('inbound', np.uint64)])
 
-def load(fname):
-    """load numpy data from jsontab file"""
-    with open(fname) as f:
-        it = (((i[0]), i[1][0], i[1][1]) for i in jsontab.iterload(f))
-        return np.fromiter(it, dtype = dtype)
+def load(dirname):
+    """load numpy data from jsontab files"""
+    it = (((i[0]), i[1][0], i[1][1]) for i in jsontab.iterload_dir(dirname))
+    return np.fromiter(it, dtype = dtype)
+
--- bluesmote/jsontab.py
+++ bluesmote/jsontab.py
@@ -1,11 +1,17 @@
 """utilities for working with MRJob's tab-separated JSON data"""
 
 import json
+import os.path
+import itertools
 
 def iterload(f):
     for line in f:
         k, v = line.rstrip().split('\t', 1)
         yield json.loads(k), json.loads(v)
+
+def iterload_dir(dirname):
+    files = [os.path.join(dirname, f) for f in os.listdir(dirname)]
+    return itertools.chain(*[iterload(open(f)) for f in files])
 
 def load(f):
     return list(iterload(f))
