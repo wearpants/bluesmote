d3af8fa1842086f38d804b815caef875be216f05
Peter Fein <pete@wearpants.org>
2013-11-07 16:30:23 -0600

    blah blah fix

--- jobs/domain_rate.py
+++ jobs/domain_rate.py
@@ -3,11 +3,17 @@
 
 from mrjob.job import MRJob
 from bluesmote.record import Record
+from mrjob.protocol import RawValueProtocol, PickleProtocol
 import re
+from itertools import imap, chain
 
 ip_re = re.compile(r"""^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$""")
 
 class DomainRate(MRJob):
+
+    INTERNAL_PROTOCOL = PickleProtocol
+    OUTPUT_PROTOCOL = RawValueProtocol
+
     @Record.wrap
     def mapper(self, _, r):
         yield "<TOTAL>",r.sc_filter_result
@@ -28,7 +34,7 @@ class DomainRate(MRJob):
                 no += 1
             else:
                 self.increment_counter("unknown_sc_filter_result", r)
-        yield domain, (yes, no, total)
+        yield domain, [yes, no, total]
 
     def reducer(self, domain, counts):
         yes = no = total = 0
@@ -37,7 +43,7 @@ class DomainRate(MRJob):
             no += n
             total += t
 
-        yield domain, yes, no, total, no/float(total)
+        yield domain, "\t".join(imap(str, (domain, yes, no, total, no/float(total))))
 
 if __name__ == '__main__':
     DomainRate.run()
