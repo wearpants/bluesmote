752a947081d767f220b087fb596290e17339c4c1
Peter Fein <pete@wearpants.org>
2013-11-06 18:49:34 -0600

    pandas

--- analysis/allthings.py
+++ analysis/allthings.py
@@ -2,6 +2,7 @@ from bluesmote import jsontab
 import os.path
 import itertools
 import numpy as np
+import pandas as pd
 
 dtype = np.dtype([('timestamp', 'datetime64[s]'),
                   ('domain', 'S128'),
@@ -15,4 +16,4 @@ def load(dirname):
     lines = itertools.chain(*(open(f) for f in files))
     raw  = (l.rstrip().split('\t') for l in lines)
     fields = ((ts, np.string_(d), a, b, i, o) for ts, d, a, b, i, o in raw)
-    return np.fromiter(fields, dtype)
+    return pd.DataFrame(np.fromiter(fields, dtype))
