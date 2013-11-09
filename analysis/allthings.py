9133dc4bd1c7c8f47bf75cd81385ad507e2a2caa
Peter Fein <pete@wearpants.org>
2013-11-08 18:43:29 -0600

    that's subtle idempotence

--- analysis/allthings.py
+++ analysis/allthings.py
@@ -8,8 +8,6 @@ dtype = np.dtype([('timestamp', 'datetime64[s]'),
                   ('domain', 'S128'),
                   ('accessed', np.uint64),
                   ('blocked', np.uint64),
-                  ('other', np.uint32),
-                  ('error', np.uint32),
                   ('in_bytes', np.uint64),
                   ('out_bytes', np.uint64)])
 
@@ -17,5 +15,5 @@ def load(dirname):
     files = [os.path.join(dirname, f) for f in os.listdir(dirname)]
     lines = itertools.chain(*(open(f) for f in files))
     raw  = (l.rstrip().split('\t') for l in lines)
-    fields = ((ts, np.string_(d), a, b, x, e, i, o) for ts, d, a, b, x, e, i, o in raw)
+    fields = ((ts, np.string_(d), a, b, i, o) for ts, d, a, b, i, o in raw)
     return pd.DataFrame(np.fromiter(fields, dtype))
