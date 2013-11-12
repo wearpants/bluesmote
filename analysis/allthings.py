6fda070386088a17fbe96c9f3f7f8d8db5ccaf25
Peter Fein <pete@wearpants.org>
2013-11-11 18:37:58 -0600

    minor

--- analysis/allthings.py
+++ analysis/allthings.py
@@ -5,11 +5,9 @@ import numpy as np
 import pandas as pd
 
 dtype = np.dtype([('timestamp', 'datetime64[s]'),
-                  ('domain', 'S128'),
+                  ('domain', 'U128'),
                   ('accessed', np.uint64),
                   ('blocked', np.uint64),
-                  ('other', np.uint32),
-                  ('error', np.uint32),
                   ('in_bytes', np.uint64),
                   ('out_bytes', np.uint64)])
 
@@ -17,5 +15,5 @@ def load(dirname):
     files = [os.path.join(dirname, f) for f in os.listdir(dirname)]
     lines = itertools.chain(*(open(f) for f in files))
     raw  = (l.rstrip().split('\t') for l in lines)
-    fields = ((ts, np.string_(d), a, b, x, e, i, o) for ts, d, a, b, x, e, i, o in raw)
+    fields = ((ts, np.string_(d), a, b, i, o) for ts, d, a, b, i, o in raw)
     return pd.DataFrame(np.fromiter(fields, dtype))
