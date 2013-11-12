18b9f9762cd4b48883b330e5017d71a279d9c647
Peter Fein <pete@wearpants.org>
2013-11-11 18:43:57 -0600

    cleanup

--- analysis/bandwidth.py
+++
@@ -1,9 +0,0 @@
-from bluesmote import jsontab
-import numpy as np
-
-dtype = np.dtype([('timestamp', 'datetime64[s]'), ('outbound', np.uint64), ('inbound', np.uint64)])
-
-def load(dirname):
-    """load numpy data from jsontab files"""
-    it = (((i[0]), i[1][0], i[1][1]) for i in jsontab.iterload_dir(dirname))
-    return np.fromiter(it, dtype = dtype)
