0cf4945836461c8117eb883f0386165df870bd9c
Peter Fein <pete@wearpants.org>
2013-11-24 11:07:36 -0600

    rescue old correct code

--- jobs/timestamp_domain.py
+++ jobs/timestamp_domain.py
@@ -5,7 +5,7 @@ from mrjob.job import MRJob
 from mrjob.protocol import RawValueProtocol, PickleProtocol
 from bluesmote.record import Record
 import re
-from itertools import imap, chain
+from itertools import imap, chain, izip_longest
 
 ip_re = re.compile(r"""^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$""")
 
@@ -20,47 +20,29 @@ class TimestampDomain(MRJob):
 
     @Record.wrap
     def mapper(self, _, r):
-        isodate = "%sT%s:00:00Z"%(r.date, r.time[:-6])
-        value = (str(r.sc_filter_result), int(r.sc_bytes), int(r.cs_bytes))
-
-        if ip_re.match(r.cs_host):
-            yield (isodate, r.cs_host), value
+        if r.sc_filter_result in ("OBSERVED", "PROXIED"):
+            accessed = 1
+            blocked = 0
+        elif r.sc_filter_result == "DENIED":
+            accessed = 0
+            blocked = 1
         else:
-            yield (isodate, ".".join(r.cs_host.rsplit('.', 2)[-2:])), value
+            self.increment_counter("unknown_sc_filter_result", r.sc_filter_result)
 
-    def combiner(self, key, value):
-        accessed = blocked = other = error = in_bytes = out_bytes = 0
-        for _ in value:
-            try:
-                r, i, o = _
-            except ValueError:
-                self.increment_counter("combiner_value_error", "XXX"+repr(_)+"XXX")
-                error += 1
-            else:
-                in_bytes += i
-                out_bytes += o
-                if r in ("OBSERVED", "PROXIED"):
-                    accessed += 1
-                elif r == "DENIED":
-                    blocked += 1
-                else:
-                    other += 1
-                    self.increment_counter("unknown_sc_filter_result", r)
+        values = (accessed, blocked, int(r.sc_bytes), int(r.cs_bytes))
 
-        assert accessed + blocked + other + error > 0, "WTF %r ==> %r"%(key, value)
-        yield key, (accessed, blocked, other, error, in_bytes, out_bytes)
+        isodate = "%sT%s:00:00Z"%(r.date, r.time[:-6])
+        if ip_re.match(r.cs_host):
+            yield (isodate, r.cs_host), values
+        else:
+            yield (isodate, ".".join(r.cs_host.rsplit('.', 2)[-2:])), values
 
-    def reducer(self, key, value):
-        accessed = blocked = other = error = in_bytes = out_bytes = 0
-        for a, b, x, e, i, o in value:
-            accessed += a
-            blocked += b
-            other += x
-            error += e
-            in_bytes += i
-            out_bytes += o
+    def combiner(self, key, values):
+        yield key, list(imap(sum, izip_longest(*values)))
 
-        yield key, "\t".join(chain(key, imap(str, (accessed, blocked, other, error, in_bytes, out_bytes))))
+    def reducer(self, key, values):
+        arr = imap(sum, izip_longest(*values))
+        yield key, "\t".join(chain(key, imap(str, arr)))
 
 
 if __name__ == '__main__':
